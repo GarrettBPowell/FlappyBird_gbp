ab778dc4f46de2db657243d1d128186f43be00c3
GarrettBPowell <garettbpowell@gmail.com>
2021-02-07 18:53:57 -0600

    changed font

--- Assets/Flappy Bird Style/Scripts/ColumnPool.cs
+++ Assets/Flappy Bird Style/Scripts/ColumnPool.cs
@@ -3,16 +3,16 @@ using System.Collections;
 
 public class ColumnPool : MonoBehaviour 
 {
-	public GameObject columnPrefab;									
-	public int columnPoolSize = 5;									
-	public float spawnRate = 3f;									
-	public float columnMin = -1f;									
-	public float columnMax = 3.5f;									
+	public GameObject columnPrefab;									//The column game object.
+	public int columnPoolSize = 5;									//How many columns to keep on standby.
+	public float spawnRate = 3f;									//How quickly columns spawn.
+	public float columnMin = -1f;									//Minimum y value of the column position.
+	public float columnMax = 3.5f;									//Maximum y value of the column position.
 
-	private GameObject[] columns;									
-	private int currentColumn = 0;									
+	private GameObject[] columns;									//Collection of pooled columns.
+	private int currentColumn = 0;									//Index of the current column in the collection.
 
-	private Vector2 objectPoolPosition = new Vector2 (-15,-25);		
+	private Vector2 objectPoolPosition = new Vector2 (-15,-25);		//A holding position for our unused columns offscreen.
 	private float spawnXPosition = 10f;
 
 	private float timeSinceLastSpawned;
@@ -21,16 +21,19 @@ public class ColumnPool : MonoBehaviour
 	void Start()
 	{
 		timeSinceLastSpawned = 0f;
-		columns = new GameObject[columnPoolSize];
 
+		//Initialize the columns collection.
+		columns = new GameObject[columnPoolSize];
+		//Loop through the collection... 
 		for(int i = 0; i < columnPoolSize; i++)
 		{
+			//...and create the individual columns.
 			columns[i] = (GameObject)Instantiate(columnPrefab, objectPoolPosition, Quaternion.identity);
 		}
 	}
 
 
-	//Keep spawning columns
+	//This spawns columns as long as the game is not over.
 	void Update()
 	{
 		timeSinceLastSpawned += Time.deltaTime;
@@ -38,8 +41,14 @@ public class ColumnPool : MonoBehaviour
 		if (GameControl.instance.gameOver == false && timeSinceLastSpawned >= spawnRate) 
 		{	
 			timeSinceLastSpawned = 0f;
+
+			//Set a random y position for the column
 			float spawnYPosition = Random.Range(columnMin, columnMax);
+
+			//...then set the current column to that position.
 			columns[currentColumn].transform.position = new Vector2(spawnXPosition, spawnYPosition);
+
+			//Increase the value of currentColumn. If the new size is too big, set it back to zero
 			currentColumn ++;
 
 			if (currentColumn >= columnPoolSize) 
